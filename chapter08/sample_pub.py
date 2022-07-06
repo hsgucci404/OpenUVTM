b0299865e58a8bcfc81dc4e5d5cac06108a2e305
Hiroe HASHIGUCHI <55542434+hsgucci404@users.noreply.github.com>
2022-07-06 14:47:40 +0900

    Update sample_pub.py

--- chapter08/sample_pub.py
+++ chapter08/sample_pub.py
@@ -9,7 +9,7 @@ def on_connect(client, userdata, flag, rc):
   print("Connected with result code " + str(rc))
 
 # ブローカーが切断したときの処理
-def on_disconnect(client, userdata, flag, rc):
+def on_disconnect(client, userdata, rc):
   if rc != 0:
      print("Unexpected disconnection.")
 
